6015a3368628295b66226bbaab2a763ff6a4c28d
Josel Xavier <joseltech2002@gmail.com>
2023-05-22 16:51:28 +0530

    backend completed

--- Login authentication/LoginAuthenticationBackend/src/main/java/com/bmc/LoginAuthenticationBackend/Service/Impl/EmployeeImpl.java
+++ Login authentication/LoginAuthenticationBackend/src/main/java/com/bmc/LoginAuthenticationBackend/Service/Impl/EmployeeImpl.java
@@ -10,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class EmployeeImpl implements EmployeeService {
     @Autowired
@@ -31,6 +33,29 @@ public class EmployeeImpl implements EmployeeService {
 
     @Override
     public LoginResponse loginEmployee(LoginDTO loginDTO) {
-        return null;
+
+        String msg = "";
+        Employee employee1 = employeeRepo.findByEmail(loginDTO.getEmail());
+        if (employee1 != null) {
+            String password = loginDTO.getPassword();
+            String encodedPassword = employee1.getPassword();
+            Boolean isPwdRight = passwordEncoder.matches(password, encodedPassword);
+            if (isPwdRight) {
+                Optional<Employee> employee = employeeRepo.findOneByEmailAndPassword(loginDTO.getEmail(), encodedPassword);
+                if (employee.isPresent()) {
+                    return new LoginResponse("Login Success", true);
+                } else {
+                    return new LoginResponse("Login Failed", false);
+                }
+            } else {
+
+                return new LoginResponse("password Not Match", false);
+            }
+        }else {
+            return new LoginResponse("Email not exits", false);
+        }
+
+
+
     }
 }
